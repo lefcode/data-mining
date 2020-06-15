8e83f3e88d284cbb0bbe30d2d5654a7468415b9f
LefterisMantas <44322307+LefterisMantas@users.noreply.github.com>
2020-06-15 19:38:27 +0300

    Delete thirtythree.py

--- wine-quality-SVM/thirtythree.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-
-data = pd.read_csv('winequality-red.csv')
-ph = data['pH'].values #retrieve ph column
-ph = np.random.choice(ph.astype(int),size =int(ph.size*0.66),replace=False,p = None)
