a8c2d69c71c949f22aeb579df5aff598fe3ee346
roustask <kostasblackoutas@hotmail.gr>
2020-06-19 11:17:32 +0300

    Deployed model

--- onion-or-not-NN/model.py
+++ onion-or-not-NN/model.py
@@ -1,11 +1,17 @@
 import string
 
 import nltk
+import numpy as np
 import pandas as pd
+import tensorflow as tf
+from keras.layers.core import Dense
+from keras.models import Sequential
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
 
-df = pd.read_csv('data/onion-or-not.csv')
-data = pd.DataFrame(df)
-rawData = data['text']
+csv = pd.read_csv('data/onion-or-not.csv')
+df = pd.DataFrame(csv)
+rawData = df['text']
 
 
 # Tokenizer function. Returns tokenized data
@@ -61,3 +67,43 @@ def preprocessData(rawContents):
     cleaned = removePunctuationFromTokenized(cleaned)
     cleaned = convertItemsToLower(cleaned)
     return cleaned
+
+
+def identity_tokenizer(text):
+    return text
+
+
+def prepare_input():
+    clean = []
+    for i in range(len(rawData)):
+        clean.append((preprocessData(rawData[i])))
+    clean = [list(clean[i]) for i in range(len(clean))]
+
+    tfidf = TfidfVectorizer(tokenizer=identity_tokenizer, stop_words='english', lowercase=False)
+
+    allData = tfidf.fit_transform(clean).toarray()
+    return allData
+
+
+model = Sequential()
+model.add(Dense(15, input_shape=(20237,), activation='relu'))
+model.add(Dense(20, activation='relu'))
+model.add(Dense(15, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+print(model.summary())
+
+# TODO optimize model
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=[tf.keras.metrics.Accuracy(), tf.keras.metrics.Recall(), tf.keras.metrics.Precision()])
+
+x = prepare_input()
+y = np.array(df['label']).transpose()
+
+X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.25)
+
+history = model.fit(X_train, Y_train, epochs=500,
+                    batch_size=32, verbose=2, validation_data=(X_test, Y_test))
+
+# Evaluate model
+scores = model.evaluate(X_test, Y_test, verbose=0)
